bebf2ab1af42991e8ad18b2d294bc70a48e4b3a8
Andrey rachitsky <>
2022-03-24 16:08:46 +0300

    основа

---
+++ SwiftUIStudy/SwiftUIStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIStudy
+//
+//  Created by Andrey Rachitskiy on 24.03.2022.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ SwiftUIStudy/SwiftUIStudy/SwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyApp.swift
+//  SwiftUIStudy
+//
+//  Created by Andrey Rachitskiy on 24.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
